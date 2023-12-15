a9800f98cac4c04fc8d0e23ec3488b4700c8d9a7
gablovicco <150941149+gablovicco@users.noreply.github.com>
2023-12-15 22:30:50 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int solution(vector<int> years)
+ {
+    int timeneeded = 0;
+
+for(int i = 1; i < years.size(); i++){
+
+    if(years[i-1] < years[1]){
+        timeneeded += 1;
+    }else if (years[i-1] > years[i]){
+        timeneeded += 2;
+
+    }else;
+
+}
+cout << timeneeded << endl;
+return timeneeded;
+
+}
+int main(){
+    vector<int> example ;
+    cout <<"Enter the number of integers: ";
+    int n;
+    cin >> n;
+    cout << "Enter " << n << " integers, separated by spaces:\n";
+    for (int i = 0; i < n; i++){
+    int num;
+    cin >> num;
+    example.push_back(num);
+    }
+
+
+    solution(example);
+}
